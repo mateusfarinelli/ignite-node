d2b36b9993db7fd76db65bb500921dcfd7d2f10f
Mateus Farinelli Zardo <mateusfarinellizardo@gmail.com>
2021-09-02 21:51:51 -0300

    Iniciando com stream de dados com a lib nativa do node filesystem ('s') e leitura de arquivos CSV com a lib csv-parse

--- chapter_II/criando_api_nodejs/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ chapter_II/criando_api_nodejs/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,34 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUserCase {
-  execute(file: any) {
-    console.log(file);
+  /**
+   * Tipo do file foi extraido do Controller, parando o mouse sobre a variavel "file"
+   * recebida através da desestruturação do request.
+   */
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    /**
+     * Lendo os dados do CSV
+     */
+    const parseFile = csvParse();
+
+    /**
+     * Pega o que está sendo lido e passa o valor lido para um outro local
+     * seja uma variavel, função e etc;
+     *
+     * Neste caso cada linha lida será passarada para a variavel parseFile
+     * que por sua vez irá fazer o parse;
+     */
+    stream.pipe(parseFile);
+
+    /**
+     * Aqui fazemos a leitura da linha e printamos cada uma das linhas
+     */
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
