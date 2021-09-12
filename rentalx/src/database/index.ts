77daf4afe2044fcde40469e80d9e53e70293a6fe
Mateus Farinelli Zardo <mateusfarinellizardo@gmail.com>
2021-09-12 17:33:04 -0300

    Criando e configurando nosso container de banco de dados

--- rentalx/src/database/index.ts
+++ rentalx/src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface OptionsInterface {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as OptionsInterface;
+  newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
