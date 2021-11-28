fd6ead12b82fe4ea676dfef60e8a480d385073df
Mateus Farinelli Zardo <mateusfarinellizardo@gmail.com>
2021-11-28 16:52:27 -0300

    Usuário: Entendo autenticação com JWT e Criando token do usuário

---
+++ rentalx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,68 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { UserRepositoryInterface } from "../../repositories/UserRepositoryInterface";
+
+interface RequestInterface {
+  email: string;
+  password: string;
+}
+
+interface ResponseInterface {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: UserRepositoryInterface
+  ) {}
+  async execute({
+    email,
+    password,
+  }: RequestInterface): Promise<ResponseInterface> {
+    // Verifica existencia usuário
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    // Valida senha
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    // Gerar JWT
+    /**
+     * Função sign é uma função sincrona, ou seja, não é uma promise e por isso não
+     * requer o await e recebe como parametros: [payload, secretOrPrivateKey, signOptions]
+     *
+     * Para o secretOrPrivateKey iremos gerar um md5 em um site;
+     */
+    const token = sign({}, "e4d249a260b1a8cd7e4627d3fb5e95b2", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const returnedToken: ResponseInterface = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return returnedToken;
+  }
+}
+
+export { AuthenticateUserUseCase };
