0a70eb3fc5e0d881eb9c85bd3485a4a4673ffdbd
Mateus Farinelli Zardo <mateusfarinellizardo@gmail.com>
2021-12-29 17:10:03 -0300

    Testes: criando o primeiro teste

---
+++ rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUserCase.spec.ts
@@ -0,0 +1,16 @@
+describe("Criar uma categoria", () => {
+  // Teste de exemplo - 1
+  it("Espero que 2 + 2 seja 4", () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+
+    expect(soma).toBe(resultado);
+  });
+  // Teste de exemplo - 2
+  it("Espero que 2 + 2 não seja 5", () => {
+    const soma = 2 + 2;
+    const resultado = 5;
+
+    expect(soma).not.toBe(resultado);
+  });
+});
